51d74143262a7dca34abcaac67f37dfcf303fca8
liamzebedee <liamzebedee@yahoo.com.au>
2022-02-27 16:58:48 +1100

    impl FollowGraph

--- contracts/core/FollowGraph.sol
+++ contracts/core/FollowGraph.sol
@@ -7,6 +7,13 @@ import {DataTypes} from '../libraries/DataTypes.sol';
 import {Events} from "../libraries/Events.sol";
 import {LensHub} from "./LensHub.sol";
 
+/**
+ * @title FollowGraph
+ * @author Anno Protocol
+ *
+ * @notice This contract implements an efficient follow graph for Lens profiles.
+ * The graph is composed of nodes (profiles) and edges between nodes.
+ */
 contract FollowGraph {
     LensHub public lensHub;
 
@@ -15,18 +22,37 @@ contract FollowGraph {
     }
 
     function follow(
-        uint256 profileId,
-        uint256 fromProfileId
+        uint256[] calldata profileIds,
+        uint256 fromProfileId,
+        bytes[] calldata datas
     ) public {
-        emit FollowEdgeChanged(fromProfileId, profileId, true);
+        for(uint256 i = 0; i < profileIds.length; i++) {
+            emit FollowEdgeChanged(fromProfileId, profileIds[i], true);
+        }
+
+        lensHub.follow(profileIds, datas);
     }
 
     function unfollow(
-        uint256 profileId,
+        uint256[] calldata profileIds,
         uint256 fromProfileId
     ) public {
-        emit FollowEdgeChanged(fromProfileId, profileId, false);
+        // We don't burn the follow NFT on purpose.
+        for(uint256 i = 0; i < profileIds.length; i++) {
+            emit FollowEdgeChanged(fromProfileId, profileIds[i], true);
+        }
     }
 
-    event FollowEdgeChanged(uint256 from, uint256 to, bool deleteOrCreate);
+    /**
+     * @dev Emitted when a follow edge is added (follow) or deleted (unfollow).
+     *
+     * @param fromProfileId The profile initiating the action (follow/unfollow).
+     * @param toProfileId The profile being followed or unfollowed.
+     * @param deleteOrCreate `false` to delete edge, `true` to create.
+     */
+    event FollowEdgeChanged(
+        uint256 indexed fromProfileId, 
+        uint256 indexed toProfileId, 
+        bool deleteOrCreate
+    );
 }
